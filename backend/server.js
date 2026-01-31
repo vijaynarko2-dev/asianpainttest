c25a18cb8d68de32bc9909a8c0f1fd14a5ea9f19
102vansh <0201it211102@gmail.com>
2026-01-31 15:28:47 +0530

    cors implementation

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const path = require("path");
+const cors = require("cors");
 const router = require("./routes/router");
 const cookieParser = require('cookie-parser');
 dotenv.config({
@@ -11,7 +12,13 @@ const connectdb = require("./db/conn")
 const app = express();
 
 const PORT = process.env.PORT;
+app.use(cors({
+    origin: 'http://localhost:5173',
 
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/auth/v1", router);
