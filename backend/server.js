7a1fb109f841d85a9b5d37dfc972d8ee24a4b72a
vijaynarko2-dev <vijaynarko2@gmail.com>
2026-02-08 20:39:09 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const app = express();
 
 const PORT = process.env.PORT;
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: true,
 
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true,
